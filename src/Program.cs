addf4c233e104fc4a97695a420bc570dcb480595
Luján <lujiuhalde@hotmail.com>
2026-03-25 19:49:48 -0300

    agregue al mesero

--- src/Program.cs
+++ src/Program.cs
@@ -17,17 +17,17 @@ namespace Ucu.Poo.Restaurant
             Menu menu = new Menu();
             menu.AddDish(burger);
             menu.AddDish(salad);
-            // Table oneTable = new Table(1);
-            // Waiter waiter = new Waiter("John");
-            // waiter.AssignTable(oneTable);
-            // oneTable.Ocupy();
-            // waiter.TakeOrder(oneTable, burger);
-            // Console.WriteLine(
-            //     $"La mesa {oneTable.Number} está ocupada: {oneTable.IsOccupied} " +
-            //     $"y la atiende: {waiter.Name}");
-            // oneTable.Free();
-            // Console.WriteLine(
-            //     $"La mesa {oneTable.Number} está ocupada: {oneTable.IsOccupied}");
+            Table oneTable = new Table(1);
+            Waiter waiter = new Waiter("John");
+            waiter.AssignTable(oneTable);
+            oneTable.Occupy();
+            waiter.TakeOrder(oneTable, burger);
+            Console.WriteLine(
+            $"La mesa {oneTable.Number} está ocupada: {oneTable.IsOccupied} " +
+            $"y la atiende: {waiter.Name}");
+            oneTable.Free();
+            Console.WriteLine(
+            $"La mesa {oneTable.Number} está ocupada: {oneTable.IsOccupied}");
         }
     }
 }
