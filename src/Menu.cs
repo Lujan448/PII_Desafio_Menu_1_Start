2bd5d3bab50667fbe5711f66bd8470fa8ceb5065
Luján <lujiuhalde@hotmail.com>
2026-04-08 22:53:45 -0300

    agregue comentarios a menú

--- src/Menu.cs
+++ src/Menu.cs
@@ -9,14 +9,20 @@ namespace Ucu.Poo.Restaurant
     {
         private List<Dish> dishes = new List<Dish>();
 
+        //Agrego un método para poder agregar los platos a la lista de platos.
+
         public void AddDish(Dish dish)
         {
             dishes.Add(dish);
         }
+
+        //Agregue un método para poder remover aquellos platos que esten en la lista de platos.
         public void RemoveDish(Dish dish)
         {
             dishes.Remove(dish);
         }
+
+        //Agregue un método para poder el plato por su nombre.
         public  Dish GetDishByName(string name)
         {
             return dishes.FirstOrDefault(plato => plato.Name == name);
